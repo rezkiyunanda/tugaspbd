535c3892dfb714d3e9d9496491ccfb45ae614aae
Husnil Kamil <husnil.k@gmail.com>
2018-05-02 06:34:53 +0700

    Bismillah

---
+++ database/migrations/2018_05_01_165137_create_perbaikan_ruangans_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePerbaikanRuangansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perbaikan_ruangan', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('ruangan_id')->unsigned();
+            $table->date('tanggal_pengajuan');
+            $table->integer('pengaju_id')->unsigned();
+            $table->text('alasan');
+            $table->integer('perbaikan_status')->default(0);
+            $table->date('tanggal_perbaikan')->nullable();
+            $table->date('tanggal_selesai_perbaikan')->nullable();
+            $table->timestamps();
+
+            $table->foreign('ruangan_id')->references('id')->on('ruangan');
+            $table->foreign('pengaju_id')->references('id')->on('users');
+            $table->foreign('ruangan_id')->references('id')->on('ruangan');
+            $table->foreing('peminjaman_status_id')->references('id')->on('peminjaman_status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perbaikan_ruangan');
+    }
+}
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,24 @@
+<div class="sidebar">
+    <nav class="sidebar-nav">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.html">
+                    <i class="nav-icon icon-speedometer"></i> Dashboard
+                    <span class="badge badge-primary">NEW</span>
+                </a>
+            </li>
+            <li class="nav-title">Theme</li>
+            <li class="nav-item">
+                <a class="nav-link" href="colors.html">
+                    <i class="nav-icon icon-drop"></i> Colors
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="typography.html">
+                    <i class="nav-icon icon-pencil"></i> Typography
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+</div>
